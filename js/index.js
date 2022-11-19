f56ba49731e955d942f6db936f9abaed82ceb55a
JosueJavier <josuejavierverbo@gmail.com>
2022-11-19 10:43:28 -0600

    proyecto banderas complet, mejoras agregadas

--- js/index.js
+++ js/index.js
@@ -1,25 +1,21 @@
-let banderas = document.getElementsByTagName('article');
+let banderas = document.getElementsByClassName('bandera');
 
 function mostrarBandera(){
   let bandera = document.getElementById('bandera').value;
+  ocultarBanderas();
   if(bandera == 'guatemala'){
-    ocultarBanderas();
     banderas[0].style.display = 'block';
   }
   else if(bandera == 'india'){
-    ocultarBanderas();
     banderas[1].style.display = 'block';
   }
   else if(bandera == 'bajos'){
-    ocultarBanderas();
     banderas[2].style.display = 'block';
   }
   else if(bandera == 'suecia'){
-    ocultarBanderas();
     banderas[3].style.display = 'block';
   }
   else if(bandera == 'suiza'){
-    ocultarBanderas();
     banderas[4].style.display = 'block';
   }
 }
